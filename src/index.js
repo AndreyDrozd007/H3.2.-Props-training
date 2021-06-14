0b6d9b1aa76af879580c514037e70c73fab8d94e
Andrey Drozd <drozdandrew58@gmail.com>
2021-06-14 18:44:03 +0300

    add: add solutions for task 1 and task 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
+// Task 1
+const Text = ({ name, color, fontSize, underline }) => {
+  return <div className='text' style={{ fontSize: fontSize, color: color, textDecoration: underline ? 'underline' : 'none' }}> {name} </div>
+}
+// Task 2
+const Square = ({ cssClass, label }) => {
+  return <div className={cssClass}> {label} </div>
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* Task 1 */}
+    <div className='text__container'>
+      <Text name='Regular text' fontSize={11} color='green' />
+      <Text name='Huge header' fontSize={25} color='aqua' />
+      <Text name='Danger notifocation' fontSize={13} color='red' />
+      <Text name='Underlined text' fontSize={16} color='grey' underline={true} />
+    </div>
+    {/* Task 2 */}
+    <div className='square__container'>
+      <Square label='Big square' cssClass='big-square' />
+      <Square label='A less square' cssClass='less-square' />
+      <Square label='The smallest one' cssClass='small-square' />
+    </div>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
